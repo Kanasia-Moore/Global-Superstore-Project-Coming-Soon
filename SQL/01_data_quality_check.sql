208a68ed435bc6dfaf7394e3396037e714beafc7
Kanasia Moore <moorek526@gmail.com>
2026-02-25 13:23:01 -0500

    Implement data quality checks in SQL script
    Added SQL queries for data quality checks on orders.

--- SQL/01_data_quality_check.sql
+++ SQL/01_data_quality_check.sql
@@ -1,1 +1,20 @@
-placeholder...
+```sql
+  SELECT  
+  Count(Order_ID) as total_orders  
+FROM `inspiring-grove-457423-b0.global_superstore.orders`;
+
+SELECT 
+  count(DISTINCT Order_ID) as bundled_orders
+FROM `inspiring-grove-457423-b0.global_superstore.orders`;
+
+SELECT 
+  min(Order_Date),
+  max(Order_Date)
+FROM `inspiring-grove-457423-b0.global_superstore.orders`;
+
+SELECT Row_ID, count(*)
+FROM `inspiring-grove-457423-b0.global_superstore.orders`
+GROUP BY Row_ID
+HAVING COUNT(*) > 1;
+
+
