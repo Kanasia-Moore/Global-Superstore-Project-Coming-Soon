6465691ccb77466a42e9b0e3aa528829ad450ea3
Kanasia Moore <moorek526@gmail.com>
2026-03-09 16:20:13 -0400

    Update 05_customer_retention.sql

--- SQL/05_customer_retention.sql
+++ SQL/05_customer_retention.sql
@@ -1,1 +1,11 @@
-placeholder...
+-- Total Customers
+SELECT
+  COUNT(DISTINCT Customer_ID) AS Total_Customers
+FROM `inspiring-grove-457423-b0.global_superstore.superstore_base`
+
+-- Repeat Customer Rate
+
+
+
+
+
